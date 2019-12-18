9edd866e939c49486db0af9a605af1d889a0f90b
xJaquar <58757179+xJaquar@users.noreply.github.com>
2019-12-19 01:16:45 +1300

    Add files via upload

---
+++ cscoop/js/home.js
@@ -0,0 +1,17 @@
+﻿function role(role) {
+    console.log(role);
+}
+
+// Calls aboutLoad when page is loaded
+$(document).ready(function(e) { 
+    socialMedia_load();
+
+});
+
+function socialMedia_load() {
+            setTimeout(function() {
+                $("#socialMediaInsert").fadeIn(2000, "linear", function() {
+                // Animation complete
+                });
+            }, 2000);
+}
